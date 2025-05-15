86764aef23f28896c5f3ab71b6b5a09f2c7969e3
azham <137070066+azhamrosli91@users.noreply.github.com>
2025-05-15 08:03:45 +0800

    Add TaskUpdate

--- MaxSystemWebSite/Interface/IUserProfile.cs
+++ MaxSystemWebSite/Interface/IUserProfile.cs
@@ -6,5 +6,7 @@ namespace MaxSys.Interface
     {
         void InitGraph(SETTING_EMAIL settings);
         Task<(bool success, string message, string? base64Image)> GetUserProfilePhotoAsync(string userEmail);
+        Task<(bool success, string message, string? base64Image)> GetUserList();
     }
+
 }
