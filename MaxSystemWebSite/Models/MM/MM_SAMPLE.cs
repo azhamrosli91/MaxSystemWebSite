8c6af95ee36588f608b3fc5ada435bf1d8212b33
Tan Jenn Horng (Maxsys) <tan_jenn_horng@toray.my>
2025-05-16 12:20:07 +0800

    Text Editor Sample V2
    Added Listing Page and Submission Page, with DB SP

--- MaxSystemWebSite/Models/MM/MM_SAMPLE.cs
+++ MaxSystemWebSite/Models/MM/MM_SAMPLE.cs
@@ -5,6 +5,8 @@ namespace MaxSystemWebSite.Models.MM
 
     public class MM_SAMPLE : BaseStandardModel
     {
+        public int ID_MM_SAMPLE { get; set; }
+        public string TITLE { get; set; }
         public string REMARK { get; set; }
     }
 
